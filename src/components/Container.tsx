0afa47d1629cd74588a5b62ebf2bcc78529f3b8e
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-11 09:52:14 -0500

    created Container/wrapper component

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import { type ElementType } from "react";
+
+type ContainerProps={
+  as: ElementType
+};
+
+const Container = ({as:Component}: ContainerProps) => {
+  
+  return (
+    <Component/>
+  )
+}
+
+export default Container
