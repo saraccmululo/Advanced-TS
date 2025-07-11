ae094de93c2744fca534379a8aecc13bda7ac6c7
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-11 11:06:32 -0500

    added forwardRef type to Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
+import { useRef } from "react";
 import { Button } from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const inputRef=useRef<HTMLInputElement>(null);
   return (
     <main>
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number"/>
+      <Input id="test" label="Test" ref={inputRef}/>
         <p>
         <Button>A Button</Button>
       </p>
       <p>
         <Button href="https://google.com">A Link</Button>
       </p>
-
       <Container as={Button} onClick={()=>{}} type="button">Click me</Container>
     </main>
   );
