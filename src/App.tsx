1319544de421e616f4f10942d56471b5f5a22d34
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-11 09:51:46 -0500

    created app component

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Button } from "./components/Button";
+import Container from "./components/Container";
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <main>
+      <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number"/>
+        <p>
+        <Button>A Button</Button>
+      </p>
+      <p>
+        <Button href="https://google.com">A Link</Button>
+      </p>
+
+      <Container as={Button}/>
+    </main>
+  );
+}
+
+export default App;
