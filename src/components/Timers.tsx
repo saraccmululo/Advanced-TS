9e176b688ac5d63a932dd75ac8bf369139644458
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-14 13:53:19 -0500

    rendered list item dynamically with .map()

--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,7 +1,12 @@
+import { useTimersContext } from "./store/timers-context"
+import Timer from "./Timer";
 
 const Timers = () => {
+  const {timers}=useTimersContext();
   return (
-    <ul>{}</ul>
+    <ul>{timers.map(timer=><li key={timer.name}>
+      <Timer {...timer}/> {/* THE SAME AS: name={timer.name} duration={timer.duration} */}
+      </li>)}</ul>
   )
 }
 
